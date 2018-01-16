9c6ab42e0378d6dce1fd6e73b54a81badd26ce01
Frits Jalvingh <jal@etc.to>
2018-01-17 00:29:38 +0100

    Try a dsl for creating a page.

---
+++ kotlin-support/src/main/java/to/etc/domui/kotlin/ui/uidsl.kt
@@ -0,0 +1,36 @@
+package to.etc.domui.kotlin.ui
+
+import to.etc.domui.component.layout.TabPanel
+import to.etc.domui.dom.html.Div
+import to.etc.domui.dom.html.NodeContainer
+import to.etc.domui.dom.html.Span
+
+/**
+ * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
+ * Created on 16-1-18.
+ */
+fun NodeContainer.div(css: String  ="", init: NodeContainer.() -> Unit): Div {
+	val d = Div()
+	add(d)
+	if(css.length > 0)
+		d.addCssClass(css)
+	d.init()
+	return d
+}
+
+fun NodeContainer.span(text: String="", init: Span.() -> Unit = {}): Span {
+	val d = Span()
+	add(d)
+	if(text.length > 0)
+		d.add(text)
+	d.init()
+	return d
+}
+
+fun NodeContainer.tabPanel(init: TabPanel.() -> Unit) : TabPanel {
+	val t = TabPanel()
+	add(t)
+	t.init()
+	return t
+}
+
--- to.etc.domui.demo/src/main/java/to/etc/domuidemo/pages/kotlin/basic/BasicPage.kt
+++ to.etc.domui.demo/src/main/java/to/etc/domuidemo/pages/kotlin/basic/BasicPage.kt
@@ -1,88 +1,26 @@
 package to.etc.domuidemo.pages.kotlin.basic
 
-import to.etc.domui.dom.html.Span
 import to.etc.domui.dom.html.UrlPage
-import to.etc.webapp.query.QCriteria
-import kotlin.reflect.KFunction1
-import kotlin.reflect.KProperty1
+import to.etc.domui.kotlin.ui.div
+import to.etc.domui.kotlin.ui.span
+import to.etc.domui.kotlin.ui.tabPanel
 
 /**
  * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
  * Created on 21-11-17.
  */
 class KtPage : UrlPage() {
-	var myName : String = ""
+	var myName: String = ""
 
 
 	override fun createContent() {
-		add(Span("Hello, world"))
+		div(css="kt-test") {
+			span(text="Hello, world")
 
-//		val q = QCriteria.create(Artist::class.java)
-////				.eq(Artist::getName, 123.34)
-//				.eq(Artist::getId, 123)
-//
-//		val kFunction1: KFunction1<Artist, String> = Artist::getName
-//
-//		val name2: KProperty1<KtPage, String> = KtPage::myName
-//
-//		QCriteria.create(KtPage::class.java)
-//				.eq(KtPage::myName, 123)
-//
-//		testValue(123, "adsd")
-//
-//		testJavaClass(String::class.java, "123")
-//		testKClass(String::class, 123)
-//
-//		val prop = Prop<String>("Hi")
-//		test2(prop, "123")
-//		test2(prop, 456)
-//
-//		val prop2 = Prop2(Artist(), "Hello")
-//		test3(prop2, 12)
-//
-		eq(KtPage::myName, 123)
+			tabPanel {
 
-	}
-//
-//	private fun <V> testValue(a: V, b: V) {
-//
-//	}
-//
-//	private fun <V> test2(a: Prop<V>, b: V) {
-//	}
-//	private fun <T, V> test3(a: Prop2<T, V>, b: V) {
-//	}
-//
-//
-//	private fun <V: Any> testKClass(clz: KClass<V>, value : V) {
-//
-//	}
-//	private fun <V> testJavaClass(clz: Class<V>, value : V) {
-//
-//	}
-//
-	fun <T, V> eq(property: KProperty1<T, V>, value: V) {
+			}
 
+		}
 	}
 }
-
-private fun <T, V : Any> QCriteria<T>.eq(kProperty1: KProperty1<T, V>, value: V) {
-}
-
-private fun <T, V : Any> QCriteria<T>.eq(getter: KFunction1<T, V>, value: V): QCriteria<T> {
-	val name = getter.name
-	println("Name is " + name + ", value = " + value)
-
-	return this
-
-}
-
-class Prop<T>(v : T) {
-	val value : T = v
-}
-
-class Prop2<T, out V>(v : T, x: V) {
-	val clz : T = v
-	val cv : V = x;
-}
-
